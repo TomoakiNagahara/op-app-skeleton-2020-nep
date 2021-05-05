f1254d2c92a099975e61243a763af62e24f519e0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2021-05-05 17:47:18 +0900

    NEW: asset > testcase > index.php, index.md

---
+++ asset/testcase/index.php
@@ -0,0 +1,14 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/testcase/index.php
+ *
+ * @created   2021-05-05
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
