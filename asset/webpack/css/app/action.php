97e6b233f2b4b2cc14e9d62faabe21bc9d64f079
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-05-28 16:31:33 +0900

    2021: asset > webpack > css > app | Copied from op-module-webpack:/css/app

---
+++ asset/webpack/css/app/action.php
@@ -0,0 +1,15 @@
+<?php
+/** op-app-skeleton-2021-nep:/asset/webpack/css/app/action.php
+ *
+ * @created   2019-11-28
+ * @moved     2021-05-28 from op-module-webpack:/css/app/action.php
+ * @version   1.0
+ * @package   op-app-skeleton-2021-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+	__DIR__.'/app',
+	__DIR__.'/language',
+];
