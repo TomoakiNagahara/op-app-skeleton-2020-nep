e91a507f6d15c32e1bd044ea7d155a4f68c1ebbe
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-05-28 16:19:14 +0900

    2021: asset > webpack > css > action.php | Copied from op-module-webpack:/css/action.php

---
+++ asset/webpack/css/action.php
@@ -0,0 +1,15 @@
+<?php
+/** op-app-skeleton-2021-nep:/asset/webpack/css/action.php
+ *
+ * @created   2018-04-17
+ * @moved     2021-05-28 from op-module-webpack:/css/action.php
+ * @version   1.0
+ * @package   op-app-skeleton-2021-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+return array_merge(
+	include(__DIR__.'/op/action.php'),
+	include(__DIR__.'/app/action.php')
+);
